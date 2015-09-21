932b1c313e14a979ec2fae538d31ae60aa4e8b76
cjblomqvist <carl@cjblomqvist.com>
2015-09-21 10:53:44 +0200

    Fix issue with keys with multiple segments not causing the proper events to trigger

--- index.js
+++ index.js
@@ -344,7 +344,18 @@ View.prototype._insertPosition = function (key, options) {
 
 function getEmit(view, id) {
   return function ret(key, path) {
+    var rootModel = view.model.root;
     var path = path || view.path + '.' + id;
+    var keySegments = key.split('.');
+    var segments = view.pathsSegments.slice(0);
+
+    while(keySegments.length > 1) {
+      segments.push(keySegments.shift());
+
+      if(rootModel._get(segments)) continue;
+
+      rootModel._set(segments, {});
+    }
 
     view.model.root.ref(view.pathsSegments.concat(key).join('.'), path);
     view.model._push(view.idsSegments.concat(id), key);
